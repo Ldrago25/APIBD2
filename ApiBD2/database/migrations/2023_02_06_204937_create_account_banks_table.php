0b66b17b137711fcd9cc415a34c35a4249d30c74
Brandon Vargas <bvargas@audisoft.com>
2023-02-06 17:43:42 -0400

    BV: implementando tablas, CMF para cada clase, relacion hasMany de usuarios a cuentas y ruta para usuarios

--- ApiBD2/database/migrations/2023_02_06_204937_create_account_banks_table.php
+++ ApiBD2/database/migrations/2023_02_06_204937_create_account_banks_table.php
@@ -13,16 +13,14 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('account_banks', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('lastName');
-            $table->string('indentification');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->unsignedBigInteger('user_id');
+            $table->integer('nro');
+            $table->string('type');
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
@@ -33,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('account_banks');
     }
 };
--- ApiBD2/database/migrations/2023_02_06_211511_create_transactions_table.php
+++ ApiBD2/database/migrations/2023_02_06_211511_create_transactions_table.php
@@ -13,15 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('transactions', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('lastName');
-            $table->string('indentification');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('form');
+            $table->integer('to');
+            $table->float('amount');
+            $table->integer('transactionType');
             $table->timestamps();
         });
     }
@@ -33,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('transactions');
     }
 };
